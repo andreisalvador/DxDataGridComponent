f3f8ec1088a392917dcb1c7a3ef16b992dd5abe4
andreisalvador <andreifs95@gmail.com>
2020-02-05 14:54:12 -0300

    Update DataSource.cs

--- Afs.DataGridComponent/Data/DataSource.cs
+++ Afs.DataGridComponent/Data/DataSource.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace Afs.DataGridComponent.Data
 {
     public class DataSource
     {
-        
+		public DataSource(IDictionary<string, object> data)
+        {
+            this.Data = new ReadOnlyDictionary<string, object>(data);            
+        }
+
+        public IReadOnlyDictionary<string, object> Data { get; set; }   
     }
 }
